f64a401aa6c92af467176ce9af64e085069d41e1
dam_ls <sumlam@alu.edu.gva.es>
2024-04-16 13:45:20 +0200

    test 3-9

--- src/es/iesmz/tests/Money.java
+++ src/es/iesmz/tests/Money.java
@@ -1,7 +1,7 @@
 package es.iesmz.tests;
 
 public class Money {
-    private static final float EurUsa = 1.18798f;
+    private static final float EurUsd = 1.18798f;
     private static final float UsdEur = 0.841815f;
     private static final float EurGbp = 0.857839f;
     private static final float GbpEur = 1.165826f;
@@ -9,20 +9,30 @@ public class Money {
 
     public static float change(TipoMoneda origen, TipoMoneda destino, float money){
         if(origen.equals(TipoMoneda.EUR)&& destino.equals(TipoMoneda.USD)){
-            money = money * EurUsa;
+            money = money * EurUsd;
         }else if(origen.equals(TipoMoneda.USD) && destino.equals(TipoMoneda.EUR)){
             money = money * UsdEur;
         } else if (origen.equals(TipoMoneda.EUR) && destino.equals(TipoMoneda.GBP)) {
             money = money * EurGbp;
         }else if (origen.equals(TipoMoneda.GBP) && destino.equals(TipoMoneda.EUR)){
             money = money * GbpEur;
-        } else if (origen.equals(destino)) {
+        }else if(origen.equals(TipoMoneda.USD)&& destino.equals(TipoMoneda.GBP)){
+            money= money * UsdEur;
+            money = money * EurGbp;
+        }else if(origen.equals(TipoMoneda.GBP)&& destino.equals(TipoMoneda.USD)){
+            money=money * GbpEur;
+            money=money * EurUsd;
+        } else if (origen.equals(TipoMoneda.PTS)&& destino.equals(TipoMoneda.EUR)){
             money = -1f;
-        } else if (money < 0f) {
+        }else if(origen.equals(TipoMoneda.EUR)&& destino.equals(TipoMoneda.PTS)){
+            money = -1f;
+        }
+        if (money <= 0f) {
             money = -1f;
         }
         return money;
     }
+
 
 
 }
--- src/es/iesmz/tests/MoneyTest.java
+++ src/es/iesmz/tests/MoneyTest.java
@@ -18,31 +18,38 @@ public class MoneyTest {
     }
     @Test
     public void change3(){
-
+        Money m3 = new Money();
+        assertEquals(201.21f,Money.change(TipoMoneda.EUR,TipoMoneda.GBP, 234.56f),0.01f);
     }
     @Test
     public void change4(){
-
+        Money m4 = new Money();
+        assertEquals(37.51f,Money.change(TipoMoneda.USD,TipoMoneda.EUR, 44.56f),0.01f);
     }
     @Test
     public void change5(){
-
+        Money m5 = new Money();
+        assertEquals(138.49f,Money.change(TipoMoneda.GBP,TipoMoneda.USD, 100.0f),0.01f);
     }
     @Test
     public void change6(){
-
+        Money m6 = new Money();
+        assertEquals(722.14f,Money.change(TipoMoneda.USD,TipoMoneda.GBP, 1000.0f),0.01f);
     }
     @Test
     public void change7(){
-
+        Money m7 = new Money();
+        assertEquals(-1f,Money.change(TipoMoneda.PTS,TipoMoneda.EUR, 100.0f), 0.01f);
     }
     @Test
     public void change8(){
-
+        Money m8 = new Money();
+        assertEquals(-1f,Money.change(TipoMoneda.EUR,TipoMoneda.PTS,123.3f),0.01f);
     }
     @Test
     public void change9(){
-
+        Money m9 = new Money();
+        assertEquals(-1f,Money.change(TipoMoneda.USD,TipoMoneda.EUR,-167.34f),0.01f);
     }
 
 }
